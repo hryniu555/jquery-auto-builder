af3f25ee27ab0935611f51aab7157da3b275669e
Vinícius Andrade <vchavesster@gmail.com>
2014-06-01 03:26:03 -0300

    also exclude sub-modules

--- bin/jquery-auto-builder.js
+++ bin/jquery-auto-builder.js
@@ -3,32 +3,47 @@
 
 "use strict";
 
-var argv = require('minimist')(process.argv.slice(2),{ boolean: "d" , string: "v" });
-var jqParser = require("../lib/jqParser");
-var jqBuilder = require("../lib/jqBuilder");
-var sys = require("util");
-//start the fun!
+var argv = require('minimist')(process.argv.slice(2),{ boolean: "d" , string: "v" }),
+	jqParser = require("../lib/jqParser"),
+	jqBuilder = require("../lib/jqBuilder"),
+	sys = require("util");
 
+if (argv._.length < 1) {
 
-if(!argv._.length > 0){
 	sys.log("No files given.");
+
 	process.exit(1);
+
 }
-if(argv.v === "1"){
+
+if (argv.v === "1") {
+
 	sys.log("Building latest 1.* jQuery stable version (1.11.1)");
+
 	argv.v = "1.11.1";//jQuery v1.* latest stable
-}else if(argv.v === "2"){
+
+} else if (argv.v === "2") {
+	
 	sys.log("Building latest 2.* jQuery stable version (2.1.1)");
+	
 	argv.v = "2.1.1"; //jQuery v2.* latest stable
-}else if(argv.v === undefined){
+
+} else if (argv.v === undefined) {
+	
 	sys.log("Building latest jQuery stable version (2.1.1)");
-}else if(argv.v !== "1.11.1" && argv.v !== "2.1.1"){
+
+} else if (argv.v !== "1.11.1" && argv.v !== "2.1.1") {
+
 	sys.log("Invalid version given. Falling back to latest (2.1.1)");
+
 	argv.v = "2.1.1";
+
 }
 
 
 function build(ver, mods){
+
+	sys.log("Ignoring modules: "+mods);
 
 	jqBuilder({
 		ver: ver,
@@ -36,21 +51,35 @@ function build(ver, mods){
 		dev: argv.d || false,
 		output: argv.o
 	}, function(err){
-		if(err){
+
+		if (err) {
+			
 			sys.error(err);
+			
 			process.exit(1);
-		}else{
+
+		} else {
+
 			sys.log("JQuery custom version built successfully!");
+
 		}
+	
 	});
+
 }
 
 jqParser.getModules(argv._, function(err, excludeMods){
+
 	if(err){
+	
 		sys.error(err);
+
 		process.exit(1);
+
 	}else{
+		
 		build(argv.v, excludeMods);
+
 	}
 });
 
